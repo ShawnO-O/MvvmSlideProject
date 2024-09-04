11ee6c73087af793e8a1f491ca2b909452310b69
Shawn <shawn760814@gmail.com>
2024-09-04 14:04:11 +0800

    Optimization

---
+++ app/src/main/java/com/shawn/mvvmslideproject/navigation/NavGraph.kt
@@ -0,0 +1,34 @@
+package com.shawn.mvvmslideproject.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.shawn.mvvmslideproject.ui.home.HomeList
+import com.shawn.mvvmslideproject.ui.profile.ProfileScreen
+import com.shawn.mvvmslideproject.ui.upload.UploadScreen
+
+@Composable
+fun NavigationConfigurations(
+    navController: NavHostController,
+    innerPadding: PaddingValues
+) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomNavItem.Home.route,
+        modifier = Modifier.padding(innerPadding)
+    ) {
+        composable(BottomNavItem.Home.route) {
+            HomeList()
+        }
+        composable(BottomNavItem.Search.route) {
+            UploadScreen()
+        }
+        composable(BottomNavItem.Profile.route) {
+            ProfileScreen()
+        }
+    }
+}
